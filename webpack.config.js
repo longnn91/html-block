fa5b8183fc7b80a8e62e7c1740d946d0c5318d80
Trinhphuong <phuong.trinh@orientsoftware.net>
2020-01-08 20:14:49 +0700

    add effect

--- webpack.config.js
+++ webpack.config.js
@@ -5,26 +5,26 @@ const path = require('path');
 
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const HtmlWebpackPluginConfig = new HtmlWebpackPlugin({
-  template: './app/index.html',
+  template: './block5/index.html',
   filename: 'index.html',
   inject: 'body'
 });
 
 const CopyWebpackPlugin = require('copy-webpack-plugin');
 const CopyWebpackPluginConfig = new CopyWebpackPlugin([
-         {from:'./app/img',to:'img'},
-         {from:'./app/fonts',to:'fonts'}
-     ]);
+  { from: './block5/img', to: 'img' },
+]);
 
 module.exports = {
-  entry: './app/index.js',
+  entry: './block5/index.js',
   output: {
     path: path.resolve('dist'),
     filename: 'index_bundle.js'
   },
   module: {
     rules: [
-      { test: /\.css$/,
+      {
+        test: /\.css$/,
         use: [
           { loader: "style-loader" },
           { loader: "css-loader" }
@@ -54,7 +54,7 @@ module.exports = {
         test: /\.woff($|\?)|\.woff2($|\?)|\.ttf($|\?)|\.eot($|\?)|\.svg($|\?)/,
         loader: 'file-loader'
       },
-       {
+      {
         test: /\.(js|jsx)$/,
         exclude: /node_modules/,
         use: "babel-loader"
